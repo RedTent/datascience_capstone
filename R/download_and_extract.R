2f94b4953bf329e3e1e0835afb6c882a49b6bb24
Johan van Tent <tentvanjohan@hotmail.com>
2020-03-15 12:06:29 +0100

    commit

--- R/download_and_extract.R
+++ R/download_and_extract.R
@@ -1,6 +1,6 @@
 download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip", 
               "data/Coursera-SwiftKey.zip")
-
+dir.create("data")
 unzip("data/Coursera-SwiftKey.zip", junkpaths = TRUE, exdir = "data")
 
 
